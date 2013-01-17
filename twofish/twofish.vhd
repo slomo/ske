2a355b9f5efc2da4a473c6708a93856ee3322eff
yves <steve.harrison@gmx.net>
2013-01-17 16:09:32 +0100

    fix a lot of little errors in new implementation

--- twofish/twofish.vhd
+++ twofish/twofish.vhd
@@ -1,10 +1,23 @@
---
 -- package twofish
 -- ===============
 --
--- consult those papers:
+-- Copyright (c) 2013, Alexander (alex@spline.de) and Yves (uves@spline.de)
+--
+-- Permission to use, copy, modify, and/or distribute this software for any purpose with
+-- or without fee is hereby granted, provided that the above copyright notice and this
+-- permission notice appear in all copies.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO
+-- THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO
+-- EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+-- DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+-- IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+-- CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+--
+-- Implemened after those papers (referenced in comments):
 --      [1] https://www.schneier.com/paper-twofish-paper.pdf
 --      [2] https://www.schneier.com/paper-twofish-fpga.pdf
+--
 
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -15,7 +28,8 @@ package twofish is
   constant SBOX_WIDTH : integer := 8;
   constant FUNC_WIDTH : integer := SBOX_WIDTH * 4;
   constant BLOCK_WIDTH : integer := FUNC_WIDTH * 4;
-  
+
+  -- types
   type vectorT is array (1 to 4) of unsigned(SBOX_WIDTH-1 downto 0);
   type matrixT is array (1 to 4) of vectorT;
 
@@ -25,6 +39,10 @@ package twofish is
 
   type confRowT is array ( 0 to 2 ) of integer range 0 to 1;
   type confT is array( 0 to 3 ) of confRowT;
+
+  type halfBlockT is array ( 0 to 1 ) of unsigned( FUNC_WIDTH-1 downto 0 );
+  
+  type blockT is array ( 0 to 3 ) of unsigned( FUNC_WIDTH-1 downto 0 );
   
   constant q : qsT := (
     (
@@ -50,7 +68,6 @@ package twofish is
       )
     );
   
-  
   constant MDS_MATRIX : matrixT := (
     ( x"01", x"EF", x"5B", x"5B" ),
     ( x"5B", x"EF", x"EF", x"01" ),
@@ -58,7 +75,7 @@ package twofish is
     ( x"EF", x"01", x"EF", x"5B" )
     );
 
-  constant QTERM_CONF : confT := (
+  constant QPERM_CONF : confT := (
     ( 0, 0, 1),
     ( 1, 0, 0),
     ( 0, 1, 1),
@@ -69,23 +86,21 @@ package twofish is
   -- funcion headers
   --
   function qperm ( input: unsigned( SBOX_WIDTH-1 downto 0); 
-                   id: integer range 0 to 1 ) 
-    return unsigned( SBOX_WIDTH-1 downto 0) ;
+                   qId: integer range 0 to 1 ) 
+    return unsigned;
 
   function mds( vector: vectorT)
     return vectorT;
 
-  function g ( input, s0, s1: unsigned( FUNC_WIDTH-1 downto 0);
-               conf: confT )
-    return unsigned( FUNC_WIDTH-1 downto 0);
+  function g ( input, s0, s1: unsigned( FUNC_WIDTH-1 downto 0))
+    return unsigned;
 
   function sbox ( input, s0, s1: unsigned( SBOX_WIDTH-1 downto 0);
                   id: integer range 0 to 3 )
-    return unsigned( SBOX_WIDTH-1 downto 0);
+    return unsigned;
 
-  function h ( input, s0, s1: unsigned( FUNC_WIDTH-1 downto 0);
-               conf: confT )
-    return unsigned( FUNC_WIDTH-1 downto 0);
+  function h ( input, s0, s1: unsigned( FUNC_WIDTH-1 downto 0))
+    return unsigned;
 
 end twofish;
 
@@ -96,7 +111,7 @@ package body twofish is
   -- perform  either a q0 or q1 permutaion (see [1] 4.5.3 )  
   function qperm ( input: unsigned( SBOX_WIDTH-1 downto 0); 
                    qId: integer range 0 to 1 ) 
-    return unsigned( SBOX_WIDTH-1 downto 0) is
+    return unsigned is
     
     variable a0, b0, a1, b1 :  unsigned( SBOX_WIDTH/2-1 downto 0);
     
@@ -140,14 +155,14 @@ package body twofish is
   -- Compute the inner Function g based directly on the Q-pertmutation. This
   -- Function is compatible to h (see [2] fig 2)
   function g ( input, s0, s1: unsigned( FUNC_WIDTH-1 downto 0))
-    return unsigned( FUNC_WIDTH-1 downto 0) is
+    return unsigned is
 
+    type sT is array ( 0 to 1 ) of unsigned (FUNC_WIDTH-1 downto 0);
     variable intern : unsigned( FUNC_WIDTH-1 downto 0);
     variable mdsVec : vectorT;
+    variable s : sT := ( s0, s1 );
     
   begin
-
-    s := ( s0, s1 );
 
     -- apply two first columns of sboxes
     for i in 0 to 1 loop 
@@ -162,7 +177,7 @@ package body twofish is
 
     -- apply last columns of sbox
     for j in 0 to 3 loop 
-      mdsVec(j) :=  qperm(intern(SBOX_WIDTH*(j-1)-1 downto SBOX_WIDTH*j), QPERM_CONF(i)(2));
+      mdsVec(j) :=  qperm(intern(SBOX_WIDTH*(j-1)-1 downto SBOX_WIDTH*j), QPERM_CONF(j)(2));
     end loop;
     
     mdsVec := mds(mdsVec);        
@@ -171,7 +186,7 @@ package body twofish is
       intern(SBOX_WIDTH*(j-1)-1 downto SBOX_WIDTH*j) := mdsVec(j); 
     end loop;
 
-    return inter;
+    return intern;
 
   end function g;
 
@@ -181,9 +196,9 @@ package body twofish is
   -- which sbox is needed ( see [2] fig 2).
   function sbox ( input, s0, s1: unsigned( SBOX_WIDTH-1 downto 0);
                   id: integer range 0 to 3 )
-    return unsigned( SBOX_WIDTH-1 downto 0) is
+    return unsigned is
 
-    type sT is array ( 0 to 2 ) of unsigned( SBOX_WIDTH-1 downto 0);
+    type sT is array ( 0 to 1 ) of unsigned( SBOX_WIDTH-1 downto 0);
     variable s: sT := ( s0, s1 );
     variable intern : unsigned( SBOX_WIDTH-1 downto 0);
   begin
@@ -209,7 +224,7 @@ package body twofish is
   -- Implements function h by using the sboxes defined before. This is a direct
   -- replacment for function g (see [2] fig 2)
   function h ( input, s0, s1: unsigned( FUNC_WIDTH-1 downto 0))
-    return unsigned( FUNC_WIDTH-1 downto 0) is
+    return unsigned is
 
     variable intern: vectorT;
   begin
@@ -220,16 +235,85 @@ package body twofish is
         input(SBOX_WIDTH*(i-1)-1 downto SBOX_WIDTH*i),
         s0(SBOX_WIDTH*(i-1)-1 downto SBOX_WIDTH*i),
         s1(SBOX_WIDTH*(i-1)-1 downto SBOX_WIDTH*i),
-        QPERM_CONF(i));
+        i);
       
     end loop;
 
     intern :=  mds( intern );
 
     return (
-      intern(0) & intern(1) & intern(2) & intern(3)
+      intern(1) & intern(2) & intern(3) & intern(4)
      );
 
   end function h;
+
+  -- pseudo havermad transform
+  --
+  --
+  function pht ( input : halfBlockT )
+    return halfBlockT is
+  begin
+    return ( input(0) + input(1), input(1) + input(0) ); 
+  end function pht;
+  
+  -- round function
+  -- 
+  -- this function is one single round
+  function round ( inBlock : blockT;
+                   rNo : integer range 0 to 15;
+                   m0, me, s : halfBlockT )
+    return blockT is
+
+    variable tmpBlock : blockT := inBlock;
+    variable roundKey : halfBlockT;
+    variable keySeed : unsigned(SBOX_WIDTH - 1 downto 0); 
+    
+  begin
+
+    -- compute keys
+    
+    -- inital round keys generate round keys
+    for i in 0 to 1 loop
+      keySeed := to_unsigned(2 * rNo + i, SBOX_WIDTH);
+
+      for j in 0 to 3 loop
+        roundKey(i) ( keySeed'length*j - 1 downto
+                      keySeed'length*(j-1) ) := keySeed;
+      end loop;
+    end loop;
+
+    
+    roundKey(0) := h(roundKey(0), m0(0), m0(1));
+    roundKey(1) := shift_left(h(roundKey(1), me(0), me(1)), 8);
+
+    roundKey := pht(roundKey);
+    roundKey(1) := shift_left(roundKey(1), 9);  
+
+    
+    -- compute rest of f
+    tmpBlock(1) := shift_left(tmpBlock(1), 8);
+
+    for i in 0 to 1 loop
+      tmpBlock(i) := g(tmpBlock(i), s(0), s(1));
+    end loop;
+
+    ( tmpBlock(0), tmpBlock(1)) := pht( (tmpBlock(0), tmpBlock(1)) );
+
+    for i in 0 to 1 loop
+      tmpBlock(i) := tmpBlock(i) xor roundKey(i);
+    end loop;
+
+    -- perform round related stuff
+
+    tmpBlock(3) := shift_left(tmpBlock(3), 1);
+
+    for i in 2 to 3 loop
+      tmpBlock(i) := tmpBlock(i) xor tmpBlock(i-2);
+    end loop;
+
+    tmpBlock(2) := shift_right(tmpBlock(2), 1);
+    
+    return tmpBlock;
+  end function round;
 
 end package body twofish;

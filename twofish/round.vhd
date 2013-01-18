1a4f0849ee13b6ab6cd2459af30fb247c13b5216
yves <steve.harrison@gmx.net>
2013-01-18 14:47:55 +0100

    add sketch for round

--- twofish/round.vhd
+++ twofish/round.vhd
@@ -5,18 +5,16 @@ use IEEE.numeric_std.all;
 entity Round is
 
   generic (
-    BLOCK_WIDTH : Integer := 128;
-    ROUND_NUMBER : Integer := 1
+    BLOCK_WIDTH : Integer := 128
     );
 
   port (
-    reset: in std_logic;
     set: in std_logic;
     clk: in std_logic;
 
 
-    keyA: in  std_logic_vector( ( BLOCK_WIDTH - 1) downto 0);
-    keyB: in  std_logic_vector( ( BLOCK_WIDTH - 1) downto 0);
+    keyA: in  std_logic_vector( ( BLOCK_WIDTH/4 - 1) downto 0);
+    keyB: in  std_logic_vector( ( BLOCK_WIDTH/4 - 1) downto 0);
     
     input: in std_logic_vector( ( BLOCK_WIDTH - 1) downto 0);
     output: out std_logic_vector( ( BLOCK_WIDTH - 1) downto 0)
@@ -26,13 +24,12 @@ end entity Round;
 
 architecture RoundArch of Round is
 
-  constant WIDTH_G : integer := BLOCK_WIDTH / 2; 
+  constant WIDTH_G : integer := BLOCK_WIDTH / 4; 
 
   component FunctionG is
     generic (
       WIDTH:  integer := WIDTH_G );
     port (
-      reset: in std_logic;
       set: in std_logic;
       clk: in std_logic;
 
@@ -43,23 +40,27 @@ architecture RoundArch of Round is
 
   type goutT is array ( 0 to 1 ) of std_logic_vector((WIDTH_G - 1) downto 0);
   signal gout : goutT;
+  
+  
+  signal gin : std_logic_vector((WIDTH_G - 1) downto 0);
     
+   
 begin
+
+  gin <=  std_logic_vector(shift_left( unsigned(input(WIDTH_G * 2 -1 downto WIDTH_G)), 8));
 
   -- handling of value 1 and 2
   gfunc0 : FunctionG port map (
     input =>  input(WIDTH_G - 1 downto 0),
     output => gout(0),
     set => set,
-    reset => reset,
     clk => clk    
     );
 
   gfunc1 : FunctionG port map (
-    input => shift_left( input(WIDTH_G * 2 - 1 downto WIDTH_G), 8),        
+    input => gin,        
     output => gout(1),
     set => set,
-    reset => reset,
     clk => clk    
     );
 
@@ -69,14 +70,15 @@ begin
   -- handling of value 3 and 4
 
   output( WIDTH_G*3 - 1 downto WIDTH_G*2) <=
-    std_logic_vector(unsigned(gout(1)) + unsigned(gout(0)) + unsigned(keyB))
-    xor shift_left( input( WIDTH_G*3 - 1 downto WIDTH_G*2), 1);
+    std_logic_vector(unsigned(gout(1)) + unsigned(gout(0)) + unsigned(keyB)
+    xor shift_left( unsigned(input( WIDTH_G*3 - 1 downto WIDTH_G*2)), 1));
 
   output( WIDTH_G*4 - 1 downto WIDTH_G*3) <=
-    shift_left(
-      std_logic_vector(unsigned(gout(1)) + unsigned(gout(0)) + unsigned(keyA))
-      xor input( WIDTH_G*4 - 1 downto WIDTH_G*3)
-      , 1); 
+    std_logic_vector(
+        shift_left(
+            unsigned(gout(1)) + unsigned(gout(0)) + unsigned(keyA)
+            xor unsigned(input( WIDTH_G*4 - 1 downto WIDTH_G*3)),
+        1)); 
   
   
 end architecture RoundArch;
